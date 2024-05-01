6f4afe45bac895e4434a3140b836986ccc2eba28
thakurvivek67 <thakurvivek6767@gmail.com>
2024-05-01 14:47:25 +0530

    Styling Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>15 August 2023</div>
-      <div>Delhi</div>
+      <div className="expense-item__location">Delhi</div>
       <div>
-        <h2>Book</h2>
-        <div>$10</div>
+        <h2 className="expense-item__description">Book</h2>
+        <div className="expense-item__price">$10</div>
       </div>
     </div>
   );
 }
+
 export default ExpenseItem;
