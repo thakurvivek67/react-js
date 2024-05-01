1765ae916166a69b4ddd3a63562b58d37803e98c
thakurvivek67 <thakurvivek6767@gmail.com>
2024-05-01 16:23:40 +0530

    style-component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+
+import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
+import "./components/ExpenseItem.css";
 
 function App() {
   return (
     <div>
       <h1>Let's get Started</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem />
     </div>
   );
 }
